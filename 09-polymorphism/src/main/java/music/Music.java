47e96d5db240d4c0e0b5449a5c9404f413778128
wujf <>
2020-12-28 18:09:42 +0800

    09-多态

---
+++ 09-polymorphism/src/main/java/music/Music.java
@@ -0,0 +1,15 @@
+package music;
+
+/**
+ * 1.0v created by wujf on 2020-12-28
+ */
+public class Music {
+  public static void tune(Instrument i){
+    i.play(Note.MIDDLE_C);
+  }
+
+  public static void main(String[] args) {
+    Wind flute = new Wind();
+    tune(flute);
+  }
+}
---
+++ 09-polymorphism/src/main/java/music/Note.java
@@ -0,0 +1,8 @@
+package music;
+
+/**
+ * 1.0v created by wujf on 2020-12-28
+ */
+public enum Note {
+  MIDDLE_C,C_SHARP,B_FLAT;
+}
